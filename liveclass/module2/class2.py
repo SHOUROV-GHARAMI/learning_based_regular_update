2bf0a8fc76c65fbb655c4b854f7bfa7240d1fab3
SHOUROV-GHARAMI <shourovgharami222@gmail.com>
2025-06-14 06:51:17 +0600

    update

---
+++ liveclass/module2/class2.py
@@ -0,0 +1,16 @@
+# a = 10
+# if a < 10:
+#     print("A is",a)
+# print("Done")
+
+# while a > 5:
+#     a = a+1
+#     print(a) 
+# print("Done! I'm finally Free!!",a)
+
+
+a = "Ha"
+while a != "HaHaHaHaHa":
+    a = a + "Ha"
+    print(a)
+print("Done")
